417d1b86991d60d6fad1b16c75699b0f502c68b1
Ariel Coppes <ariel.coppes@gmail.com>
2020-04-09 22:11:45 -0300

    started a new project to render some custom ui rulers to help design ui

---
+++ Assets/Gemserk.UICustomRulers.Editor/UIRulerCustomInspector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Gemserk.UICustomRulers.Editor
+{
+    [CustomEditor(typeof(UIRuler))]
+    public class UIRulerCustomInspector : UnityEditor.Editor
+    {
+        private void OnSceneGUI()
+        {
+            var t = target as UIRuler;
+            
+            // if (!string.IsNullOrEmpty(t.label))
+            // {
+            //     Handles.Label(t.transform.position, t.label, new GUIStyle()
+            //     {
+            //         normal = new GUIStyleState
+            //         {
+            //             textColor = t.color
+            //         }
+            //     });
+            // }
+            //
+        }
+    }
+}

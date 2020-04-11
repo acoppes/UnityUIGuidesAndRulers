d822409908b2a1d1ce4bb11b85667ef2ebbbedae
Ariel Coppes <ariel.coppes@gmail.com>
2020-04-11 11:17:12 -0300

    moving stuff to make it compatible with package for unity

---
+++ Editor/UIRulerCustomInspector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+
+namespace Gemserk.UIGuidesAndRulers.Editor
+{
+    [CustomEditor(typeof(UISimpleGuide))]
+    public class UIRulerCustomInspector : UnityEditor.Editor
+    {
+        private void OnSceneGUI()
+        {
+            var t = target as UISimpleGuide;
+            
+            // if (!string.IsNullOrEmpty(t.label))
+            // {
+            //     Handles.Label(t.transform.position, t.label, new GUIStyle()
+            //     {
+            //         normal = new GUIStyleState
+            //         {
+            //             textColor = t.color
+            //         }
+            //     });
+            // }
+            //
+        }
+    }
+}
